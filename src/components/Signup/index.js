462d4a493b9962f9688d09604afc0d3a11db5577
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2019-10-17 11:36:07 -0600

    Added react-select to work with Option Selects

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import styles from './Signup.module.scss'
+import Select from 'react-select'
+
+
+const options2 = [
+    {value: 'Saturday', label: 'Saturday'},
+    {value: 'Sunday', label: 'Sunday'},
+    {value: 'Monday', label: 'Monday'}
+]
 
 const Signup = (props) => {
     return (
@@ -16,21 +24,31 @@ const Signup = (props) => {
             <span className={styles.exerpt2}>(you can use your email to access your account & recommendations)</span>
         </div>
 
-       {/* <div className={styles.Preferences}>
+   <div className={styles.Preferences}>
             <h2>My Preferences</h2>
             Let’s learn more about what foods you and your family like and the best time to deliver.
-            <p>I don’t eat <input type="number" className={styles.formInputB} onKeyDown={props.handleFormB} name="number_of_servings"  onChange={props.getFormData } /> 
+            <p>I don’t eat <input type="text" className={styles.formInput} onKeyDown={props.handleFormB} name="number_of_servings"  onChange={props.getFormData } /> 
                 gluten, meat, other - write in
             </p> 
 
-            <p>I prefer to get my freshly pre-prepped meals on <input type="number" className={styles.formInputB} onKeyDown={props.handleFormB} name="number_of_servings"  onChange={props.getFormData } /></p>
+            <p><div className={styles.optionSelect}>
+                I prefer to get my freshly pre-prepped meals on&nbsp;                
+                    <Select options={options2} />
+                </div>
+                
+            </p>
+ 
+            
 
-             <p>What looks best to you?</p>
+              <p>What looks best to you?</p>
 
-             Images of diverse food selection w/ option to swipe right/left or select boxes in grid
-        </div>
+              Images of diverse food selection w/ option to swipe right/left or select boxes in grid
+    </div>
+
+
+
         <div className={styles.Plan}>
-            <h2>Recommended Plan</h2>
+             <h2>Recommended Plan</h2>
 
             <p>Name, we’ve selected the following plan - perfect for you/ your family.</p>
 
@@ -48,7 +66,7 @@ const Signup = (props) => {
                </div>
                SELECT THIS PLAN
            </div>
-       </div>*/}
+       </div>
     </div> 
 
     )
